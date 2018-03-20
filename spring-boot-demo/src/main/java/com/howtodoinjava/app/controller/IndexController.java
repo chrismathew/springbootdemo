dcf048ae0fb78012bbc7b874b6f68e5f9ae92602
chrismathew <cmathew06@gmail.com>
2018-03-20 08:10:00 -0400

    Add files via upload

--- spring-boot-demo/src/main/java/com/howtodoinjava/app/controller/IndexController.java
+++ spring-boot-demo/src/main/java/com/howtodoinjava/app/controller/IndexController.java
@@ -2,15 +2,51 @@ package com.howtodoinjava.app.controller;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+
+
+
+/*
+ *     private AppProperties app;
+    private GlobalProperties global;
+
+    @Autowired
+    public void setApp(AppProperties app) {
+        this.app = app;
+    }
+
+    @Autowired
+    public void setGlobal(GlobalProperties global) {
+        this.global = global;
+    }
+ */
+
 @Controller
 public class IndexController {
+	
+
+	// a SpringBoot way to retrieve property
+	
+	private DemoProperty demoProperty;
+	 private GlobalProperties global;
+	@Autowired
+	public void setDemoProperty(DemoProperty demoProperty) {
+		this.demoProperty = demoProperty;
+	}
+	
+	  @Autowired
+	    public void setGlobal(GlobalProperties global) {
+	        this.global = global;
+	    }
 
 	@RequestMapping("/")
 	public String home(Map<String, Object> model) {
-		model.put("message", "HowToDoInJava Reader !!");
+		 String globalProperties = global.toString();
+		 System.out.println("GLOBLA VALUES:" + globalProperties);
 		return "index";
 	}
 	
@@ -22,8 +58,13 @@ public class IndexController {
 	
 	@RequestMapping("/errorPage")
 	public String errorPage() {
+	/*	demoProperty.getMapProperty().get("CONTRACT_LOCKED"); 
+		demoProperty.getMapProperty().get("MEMBER_NOT_FOUND");
+		System.out.println("DEMO PROPERTY 1: " + demoProperty.getMapProperty().get("CONTRACT_LOCKED"));
 		
-		return "errorPage";
+		
+		System.out.println("VALUE OF EMAIL :" + demoProperty.getEmail());
+*/		return "errorPage";
 	}
 
 }
